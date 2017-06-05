70bcb265ee793382c0cf1108e94263a4fbff0f14
Bojan-Posao-PC\Bojan <bojan.gajic@enetelsolutions.com>
2017-06-05 22:39:50 +0200

    progress on migration from subscriber to customer

---
+++ src/at/mtel/denza/alfresco/util/Migration.java
@@ -0,0 +1,77 @@
+package at.mtel.denza.alfresco.util;
+
+import java.util.List;
+
+import javax.persistence.RollbackException;
+import javax.ws.rs.core.Response;
+
+import at.mtel.denza.alfresco.jpa.Customer;
+import at.mtel.denza.alfresco.jpa.EntityManagerSingleton;
+import at.mtel.denza.alfresco.jpa.Metadata;
+import at.mtel.denza.alfresco.jpa.Subscriber;
+import at.mtel.denza.alfresco.ws.rest.FunctionIntegrator;
+
+public class Migration {
+	private Customer c;
+	private Subscriber s;
+
+	public static void main(String[] args) {
+		Migration m = new Migration();
+		m.migrateSubscriberToCustomer("9012","1012618528");
+	}
+
+	public void migrateSubscriberToCustomer(String customer, String subscriber) {
+		List<Customer> customerList = FunctionIntegrator.getCustomer(customer);
+		if (customerList.isEmpty()) {
+			// create customer
+			c = new Customer();
+			c.setCustomerId(customer);
+			addSubscriber(c, subscriber);
+
+		} else {
+			c = customerList.get(0);
+			System.out.println(c.getId());
+			addSubscriber(c, subscriber);
+			addSubscriberToMetadata(c);
+			//System.out.println(c.getMetadata().get(0).getSubscriber());
+		}
+		try {
+			EntityManagerSingleton.getEntityManager().getTransaction().begin();
+			EntityManagerSingleton.getEntityManager().persist(c);
+			EntityManagerSingleton.getEntityManager().getTransaction().commit();
+			System.out.println("saved!");
+			// logger.warn("Customer created: " + customerId);
+		} catch (RollbackException re) {
+			System.err.println("failed!");
+		}
+
+	}
+
+	private void addSubscriber(Customer cust, String subscriberId) {
+		List<Subscriber> subsList = FunctionIntegrator.getSubscriber(subscriberId);
+
+		if (subsList.isEmpty()) {
+			Subscriber subscr = new Subscriber();
+			subscr.setSubscriberId(subscriberId);
+			subscr.setCustomer(cust);
+			subsList.add(subscr);
+			cust.setSubscribers(subsList);
+		} else {
+			Subscriber s = subsList.get(0);
+			s.setCustomer(cust);
+			cust.setSubscribers(subsList);
+		}
+	}
+
+	private void addSubscriberToMetadata(Customer cust) {
+		List<Metadata> metaList = cust.getMetadata();
+		if (!metaList.isEmpty()) {
+			for(Metadata m : metaList){
+				m.setSubscriber(cust.getSubscribers().get(0));
+			}
+			c.setMetadata(metaList);
+
+		}
+	}
+
+}
--- src/at/mtel/denza/alfresco/ws/rest/CacheWebServices.java
+++ src/at/mtel/denza/alfresco/ws/rest/CacheWebServices.java
@@ -1,25 +1,62 @@
 package at.mtel.denza.alfresco.ws.rest;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 import at.mtel.denza.alfresco.jpa.EntityManagerSingleton;
+import at.mtel.denza.alfresco.util.Migration;
 
 @Path("/alfresco")
 public class CacheWebServices {
-	
+
 	@GET
-	@Produces({ MediaType.APPLICATION_JSON }) 
+	@Produces({ MediaType.APPLICATION_JSON })
 	@Path("/cache/refresh")
 	public Response cacheRefresh() {
-		
+
 		EntityManagerSingleton.getEntityManager().clear();
-		
+
 		return Response.ok("Cleared!").build();
+	}
+
+	@GET
+	@Produces({ MediaType.APPLICATION_JSON })
+	@Path("/migration")
+	public Response migration(@QueryParam("path") String file) {
+		if (file != null && file.length() > 3) {
+			java.nio.file.Path path = Paths.get(file);
+			try {
+				Migration m = new Migration();
+				List<String> list = Files.readAllLines(path);
+				for (String s : list) {
+					String[] spl = s.split(";");
+					if (spl.length == 2) {
+						m.migrateSubscriberToCustomer(spl[0], spl[1]);
+					}
+				}
+				return Response.ok("migrated: " + list.size()).build();
+
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+
+		return Response.notModified().build();
 	}
 
 }
